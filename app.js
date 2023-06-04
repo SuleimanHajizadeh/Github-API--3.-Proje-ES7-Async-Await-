69753672defaadcdef521c1a1efa7e0f3b00035b
software1engineer1Solomon <suleyman.hacizade1@gmail.com>
2023-06-04 15:49:58 +0400

    add

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const nameInput = document.getElementById("githubname");
 const clearLastUsers = document.getElementById("clear-last-users");
 const lastUsers = document.getElementById("last-users");
 const github = new Github();
+const ui = new UI();
 
 addEventListeners();
 
@@ -28,7 +29,7 @@ function getData(e) {
                 console.log("Hata");
             }
             else {
-                console.log(response)
+                ui.showUserInfo(response.user);
             }
         })
         .catch(err => console.log(err));
@@ -36,6 +37,7 @@ function getData(e) {
 
 
 
+    ui.clearInput(); // Input Temizleme
 
     e.preventDefault();
 }
