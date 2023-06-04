8ef2c699db19c6330866ea731c1b067748fb2142
software1engineer1Solomon <suleyman.hacizade1@gmail.com>
2023-06-04 15:28:39 +0400

    Add

---
+++ app.js
@@ -0,0 +1,49 @@
+// Elementleri Secme
+const githubForm = document.getElementById("github-form");
+const nameInput = document.getElementById("githubname");
+const clearLastUsers = document.getElementById("clear-last-users");
+const lastUsers = document.getElementById("last-users");
+const github = new Github();
+
+addEventListeners();
+
+function eventListeners() {
+    githubForm.addEventListener("submit", getData);
+    clearLastUsers.addEventListener("click", clearAllSearched);
+    document.addEventListener("DOMContentLoaded", getAllSearched);
+}
+
+function getData(e) {
+
+    let username = nameInput.value.trim();
+
+    if (username === "") {
+        alert("Lutfen Gecerli bir kullanici adi girin.");
+    }
+    else {
+        github.getGithubData(username)
+        .then(response => {
+            if(response.user.message === "Not Found") {
+                // Hata Mesaji
+                console.log("Hata");
+            }
+            else {
+                console.log(response)
+            }
+        })
+        .catch(err => console.log(err));
+    }
+
+
+
+
+    e.preventDefault();
+}
+
+function clearAllSearched() {
+    // Tum arananlari temizle
+}
+
+function getAllSearched() {
+    // Arananlari Storagedan al ve UIye ekle
+}
